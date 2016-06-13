25f57e1155fba9f1bc009d787a1b267b2fc739c0
Denis Laliberte <denis.laliberte@gmail.com>
2016-06-12 21:34:24 -0400

    add get function to commands collection

--- lib/sobriquet/command_collection.rb
+++ lib/sobriquet/command_collection.rb
@@ -1,12 +1,18 @@
 module Sobriquet
-  # Command hold value and compile to executable string
+  # Contain a collection of commands
   class CommandCollection
     def initialize(workspace)
       @workspace = workspace
       @persistance = Persistance.new(workspace)
+      @commands = []
+    end
+
+    def get
+      @commands
     end
 
     def add(data)
+      @commands.push(data)
     end
   end
 end
--- spec/command_collection_spec.rb
+++ spec/command_collection_spec.rb
@@ -2,9 +2,14 @@ require 'sobriquet'
 include Sobriquet
 
 RSpec.describe CommandCollection do
-  it 'blablabla' do
+  let(:command_data) do
+    ['git status', 'gs', 'get the status of the git directory']
+  end
+
+  it 'add a new commands' do
     expect(Persistance).to receive(:new).with('sobriquet.csv')
     commands = described_class.new('sobriquet.csv')
-    commands.add(['git status', 'gs', 'get the status of the git directory'])
+    commands.add(command_data)
+    expect(commands.get).to include(command_data)
   end
 end
