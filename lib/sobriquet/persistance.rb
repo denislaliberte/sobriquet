ce80938ba87c2e3283d50cc8b4b514c53b03cae8
Denis Laliberte <denis.laliberte@gmail.com>
2016-06-14 18:13:35 -0400

    add save to persistance

--- lib/sobriquet/persistance.rb
+++ lib/sobriquet/persistance.rb
@@ -3,16 +3,26 @@ require 'CSV'
 module Sobriquet
   # Persistance handle interaction with the file system
   class Persistance
-    def initialize(workspace)
+    def initialize(workspace, title)
       @workspace = workspace
+      @title = title
     end
 
     def get
-      _title, *data = CSV.read('sobriquet.csv', 'rb', col_sep: ' | ')
+      _title, *data = CSV.read(@workspace, 'rb', col_sep: ' | ')
       data.map { |a| Command.new(a) }
     end
 
     def save(commands)
+      csv_string = CSV.generate(col_sep: ' | ') do |csv|
+        csv.add_row(@title)
+        commands.map do |line|
+          csv.add_row(line)
+        end
+      end
+      File.open(@workspace, 'w') do |file|
+        file.write(csv_string)
+      end
     end
   end
 end
--- spec/persistance_spec.rb
+++ spec/persistance_spec.rb
@@ -2,19 +2,36 @@ require 'sobriquet'
 include Sobriquet
 
 RSpec.describe Persistance do
-  let(:data) do
-    'command | alias | description
-git status | gs | get the status of the git directory'
+  let(:title) do
+    ['command','alias','description']
   end
-  let(:result) do
+  let(:csv) do
+    'command | alias | description
+"git status" | gs | "get the status of the git directory"
+'
+  end
+  let(:compiled_command) do
     '# get the status of the git directory
 alias gs="git status"
 '
   end
+  let(:data) do
+    [
+      ['git status', 'gs', 'get the status of the git directory']
+    ]
+  end
 
-  it 'retrive the command data from csv' do
-    expect(File).to receive(:open).with(any_args) { StringIO.new(data) }
-    persistance = described_class.new('workspace.csv')
-    expect(persistance.get.first.command).to eq(result)
+  it 'retrive the command csv from csv' do
+    expect(File).to receive(:open).with(any_args) { StringIO.new(csv) }
+    persistance = described_class.new('workspace.csv', title)
+    expect(persistance.get.first.command).to eq(compiled_command)
+  end
+
+  it 'save the commands data to the csv' do
+    file = spy('file')
+    expect(File).to receive(:open).with(any_args).and_yield(file)
+    persistance = described_class.new('workspace.csv', title)
+    persistance.save(data)
+    expect(file).to have_received(:write).with(csv)
   end
 end
